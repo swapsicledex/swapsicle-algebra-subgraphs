d22b9bfefd90775c7e5c7ab84dc1093e5e8a9ddb
rozzaswap <rozza@swapsicle.io>
2024-01-24 16:11:35 +0000

    Updated constants.ts

--- Algebra/src/utils/constants.ts
+++ Algebra/src/utils/constants.ts
@@ -4,9 +4,9 @@ import { Factory as FactoryContract } from '../types/templates/Pool/Factory'
 
 
 export const ADDRESS_ZERO = '0x0000000000000000000000000000000000000000'
-export const FACTORY_ADDRESS = '0xC848bc597903B4200b9427a3d7F61e3FF0553913'
-export const SLUSH_ADDRESS = '0x8309bc8bb43fb54db02da7d8bf87192355532829'
-export const ICE_ADDRESS = Address.fromString('0x005e16eccdfd3ea76e7b777a1beb7b826e3aa7e3')
+export const FACTORY_ADDRESS = '0xc074c6551a9cB8f63ff3dC2eA9616996db9B4eCF'
+export const SLUSH_ADDRESS = '0xb9c6acfaf5cb0580476a0b609d8c4f64a8c793ce'
+export const ICE_ADDRESS = Address.fromString('0x55cba56adc37639dbcda1d88c245a9544753441e')
 
 export let ZERO_BI = BigInt.fromI32(0)
 export let ONE_BI = BigInt.fromI32(1)
