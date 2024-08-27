fda4cddfd6939a38917fa60e874793970947044b
paoloswap <122459630+paoloswap@users.noreply.github.com>
2024-08-27 09:02:00 +0200

    Add transaction details to farms subgraph
    Improve farms code style & fix null issues

--- AlgebraFarming/src/mappings/eternalFarming.ts
+++ AlgebraFarming/src/mappings/eternalFarming.ts
@@ -1,4 +1,4 @@
-import { ethereum, crypto, BigInt } from '@graphprotocol/graph-ts';
+import { ethereum, crypto, BigInt } from '@graphprotocol/graph-ts'
 import {
   EternalFarmingCreated,
   FarmEntered,
@@ -9,9 +9,10 @@ import {
   RewardsRatesChanged,
   RewardsAdded,
   RewardsCollected
-} from '../types/EternalFarming/EternalFarming';
-import { Deposit, Reward, EternalFarming } from '../types/schema';
+} from '../types/EternalFarming/EternalFarming'
+import { Deposit, Reward, EternalFarming } from '../types/schema'
 import { createTokenEntity } from '../utils/token'
+import { loadTransaction } from '../utils'
 
 export function handleIncentiveCreated(event: EternalFarmingCreated): void {
   let incentiveIdTuple: Array<ethereum.Value> = [
@@ -19,66 +20,76 @@ export function handleIncentiveCreated(event: EternalFarmingCreated): void {
     ethereum.Value.fromAddress(event.params.bonusRewardToken),
     ethereum.Value.fromAddress(event.params.pool),
     ethereum.Value.fromUnsignedBigInt(event.params.nonce)
-  ];
+  ]
 
   createTokenEntity(event.params.rewardToken)
   createTokenEntity(event.params.bonusRewardToken)
   
-  let _incentiveTuple = changetype<ethereum.Tuple>(incentiveIdTuple);
+  let _incentiveTuple = changetype<ethereum.Tuple>(incentiveIdTuple)
 
   let incentiveIdEncoded = ethereum.encode(
     ethereum.Value.fromTuple(_incentiveTuple)
-  )!;
-  let incentiveId = crypto.keccak256(incentiveIdEncoded);
+  )!
+  let incentiveId = crypto.keccak256(incentiveIdEncoded)
 
-  let entity = EternalFarming.load(incentiveId.toHex()); 
+  let entity = EternalFarming.load(incentiveId.toHex())
   if (entity == null) {
-    entity = new EternalFarming(incentiveId.toHex());
-    entity.reward = BigInt.fromString("0");
-    entity.bonusReward = BigInt.fromString("0");
+    entity = new EternalFarming(incentiveId.toHex())
+    entity.bonusRewardRate = BigInt.fromString("0")
+    entity.rewardRate = BigInt.fromString("0")
+    entity.reward = BigInt.fromString("0")
+    entity.bonusReward = BigInt.fromString("0")
   }
-  
 
-  entity.rewardToken = event.params.rewardToken;
-  entity.bonusRewardToken = event.params.bonusRewardToken;
-  entity.pool = event.params.pool;
-  entity.nonce = event.params.nonce;
-  entity.virtualPool = event.params.virtualPool;
+  let transaction = loadTransaction(event)
+  entity.transaction = transaction.id
+  entity.timestamp = transaction.timestamp
 
-  entity.isDeactivated = false;
+  entity.rewardToken = event.params.rewardToken
+  entity.bonusRewardToken = event.params.bonusRewardToken
+  entity.pool = event.params.pool
+  entity.nonce = event.params.nonce
+  entity.virtualPool = event.params.virtualPool
+
+  entity.isDeactivated = false
   entity.minRangeLength = BigInt.fromI32(event.params.minimalAllowedPositionWidth)
-  entity.save();
+  entity.save()
 }
 
 
 export function handleTokenStaked(event: FarmEntered): void {
-  let entity = Deposit.load(event.params.tokenId.toString());
-  if (entity != null) {
-    entity.eternalFarming = event.params.incentiveId;
-    entity.save();
-  }
+  let entity = Deposit.load(event.params.tokenId.toString())
+  if (entity == null) return
 
+  let transaction = loadTransaction(event)
+  entity.transaction = transaction.id
+  entity.timestamp = transaction.timestamp
+  entity.eternalFarming = event.params.incentiveId
+  entity.save()
 }
 
 export function handleRewardClaimed(event: RewardClaimed): void {
-  let id = event.params.rewardAddress.toHexString() + event.params.owner.toHexString();
-  let rewardEntity = Reward.load(id);
-  if (rewardEntity != null){
-      rewardEntity.owner = event.params.owner;
-      rewardEntity.rewardAddress = event.params.rewardAddress;
-      rewardEntity.amount = rewardEntity.amount.minus(event.params.reward);
-      rewardEntity.save();
-  }
+  let id = event.params.rewardAddress.toHexString() + event.params.owner.toHexString()
+  let rewardEntity = Reward.load(id)
+  if (rewardEntity == null) return
+
+  let transaction = loadTransaction(event)
+  rewardEntity.transaction = transaction.id
+  rewardEntity.timestamp = transaction.timestamp
+  rewardEntity.owner = event.params.owner
+  rewardEntity.rewardAddress = event.params.rewardAddress
+  rewardEntity.amount = rewardEntity.amount.minus(event.params.reward)
+  rewardEntity.save()
 }
 
 export function handleTokenUnstaked(event: FarmEnded): void {
-  
-  let entity = Deposit.load(event.params.tokenId.toString());
-  
-  if(entity){
-    let eternalFarming = EternalFarming.load(entity.eternalFarming! .toHexString())
-
-    if(eternalFarming){
+  let transaction = loadTransaction(event)
+  let entity = Deposit.load(event.params.tokenId.toString())
+  if (entity) {
+    let eternalFarming = EternalFarming.load(entity.eternalFarming!.toHexString())
+    if (eternalFarming) {
+      eternalFarming.transaction = transaction.id
+      eternalFarming.timestamp = transaction.timestamp
       eternalFarming.reward -= event.params.reward
       eternalFarming.bonusReward -= event.params.bonusReward
       eternalFarming.save()
@@ -86,28 +97,29 @@ export function handleTokenUnstaked(event: FarmEnded): void {
   }
 
   if (entity != null) {
-    entity.eternalFarming = null;  
-    entity.save();
+    entity.eternalFarming = null  
+    entity.save()
   }
 
   let id = event.params.rewardAddress.toHexString() + event.params.owner.toHexString()
   let rewardEntity = Reward.load(id)
 
-  if (rewardEntity == null){
-      rewardEntity = new Reward(id)
-      rewardEntity.amount = BigInt.fromString('0')
+  if (rewardEntity == null) {
+    rewardEntity = new Reward(id)
+    rewardEntity.amount = BigInt.fromString('0')
   }
 
   rewardEntity.owner = event.params.owner
   rewardEntity.rewardAddress = event.params.rewardAddress
   rewardEntity.amount = rewardEntity.amount.plus(event.params.reward)
-  rewardEntity.save();  
+  rewardEntity.transaction = transaction.id
+  rewardEntity.timestamp = transaction.timestamp
+  rewardEntity.save()  
 
-
-  id =  event.params.bonusRewardToken.toHexString() + event.params.owner.toHexString()
+  id = event.params.bonusRewardToken.toHexString() + event.params.owner.toHexString()
   rewardEntity = Reward.load(id)
 
-  if (rewardEntity == null){
+  if (rewardEntity == null) {
     rewardEntity = new Reward(id)
     rewardEntity.amount = BigInt.fromString('0')
   }
@@ -115,78 +127,91 @@ export function handleTokenUnstaked(event: FarmEnded): void {
   rewardEntity.owner = event.params.owner
   rewardEntity.rewardAddress = event.params.bonusRewardToken
   rewardEntity.amount = rewardEntity.amount.plus(event.params.bonusReward)
-  rewardEntity.save();
-
+  rewardEntity.transaction = transaction.id
+  rewardEntity.timestamp = transaction.timestamp
+  rewardEntity.save()
 }
 
-export function handleDeactivate( event: IncentiveDeactivated): void{
+export function handleDeactivate(event: IncentiveDeactivated): void {
+  let entity = EternalFarming.load(event.params.incentiveId.toHex())
+  if (entity == null) return
 
-  let entity = EternalFarming.load(event.params.incentiveId.toHex());
-
-  if(entity){
-    entity.isDeactivated = true
-    entity.save()
-  } 
-
+  let transaction = loadTransaction(event)
+  entity.isDeactivated = true
+  entity.transaction = transaction.id
+  entity.timestamp = transaction.timestamp
+  entity.save()
 }
 
 
-export function handleRewardsRatesChanged( event: RewardsRatesChanged): void{
+export function handleRewardsRatesChanged(event: RewardsRatesChanged): void {
   let eternalFarming = EternalFarming.load(event.params.incentiveId.toHexString())
-  if(eternalFarming){
-    eternalFarming.rewardRate = event.params.rewardRate
-    eternalFarming.bonusRewardRate = event.params.bonusRewardRate
-    eternalFarming.save()
-  }
+  if (eternalFarming == null) return
+
+  let transaction = loadTransaction(event)
+  eternalFarming.rewardRate = event.params.rewardRate
+  eternalFarming.bonusRewardRate = event.params.bonusRewardRate
+  eternalFarming.transaction = transaction.id
+  eternalFarming.timestamp = transaction.timestamp
+  eternalFarming.save()
 }
 
-export function handleRewardsAdded( event: RewardsAdded): void{
+export function handleRewardsAdded(event: RewardsAdded): void {
   let eternalFarming = EternalFarming.load(event.params.incentiveId.toHexString())
-  if(eternalFarming){
-    eternalFarming.reward += event.params.rewardAmount
-    eternalFarming.bonusReward += event.params.bonusRewardAmount 
-    eternalFarming.save()
-  }
+  if (eternalFarming == null) return
+
+  let transaction = loadTransaction(event)
+  eternalFarming.reward += event.params.rewardAmount
+  eternalFarming.bonusReward += event.params.bonusRewardAmount
+  eternalFarming.transaction = transaction.id
+  eternalFarming.timestamp = transaction.timestamp
+  eternalFarming.save()
 }
 
-export function handleRewardDecreased( event: RewardAmountsDecreased): void{
+export function handleRewardDecreased(event: RewardAmountsDecreased): void {
   let eternalFarming = EternalFarming.load(event.params.incentiveId.toHexString())
-  if(eternalFarming){
-    eternalFarming.reward -= event.params.rewardAmount
-    eternalFarming.bonusReward -= event.params.bonusRewardAmount 
-    eternalFarming.save()
-  }
+  if (eternalFarming == null) return
+
+  let transaction = loadTransaction(event)
+  eternalFarming.reward -= event.params.rewardAmount
+  eternalFarming.bonusReward -= event.params.bonusRewardAmount
+  eternalFarming.transaction = transaction.id
+  eternalFarming.timestamp = transaction.timestamp
+  eternalFarming.save()
 }
 
-export function handleCollect( event: RewardsCollected): void{
+export function handleCollect(event: RewardsCollected): void {
+  let entity = Deposit.load(event.params.tokenId.toString())
+  if (entity == null) return
 
-  let entity = Deposit.load(event.params.tokenId.toString());
-  
-  if(entity){
-    let eternalFarmingID = entity.eternalFarming!.toHexString()
-    let eternalFarming = EternalFarming.load(eternalFarmingID)
+  let transaction = loadTransaction(event)
+  let eternalFarmingID = entity.eternalFarming!.toHexString()
+  let eternalFarming = EternalFarming.load(eternalFarmingID)
 
-    if(eternalFarming){
-      eternalFarming.reward -= event.params.rewardAmount
-      eternalFarming.bonusReward -= event.params.bonusRewardAmount
-      eternalFarming.save()
-    
+  if (eternalFarming == null) return
+
+  eternalFarming.reward -= event.params.rewardAmount
+  eternalFarming.bonusReward -= event.params.bonusRewardAmount
+  eternalFarming.transaction = transaction.id
+  eternalFarming.timestamp = transaction.timestamp
+  eternalFarming.save()
 
   let id = eternalFarming.rewardToken.toHexString() + entity.owner.toHexString()
   let rewardEntity = Reward.load(id)
 
   if (rewardEntity == null){
-      rewardEntity = new Reward(id)
-      rewardEntity.amount = BigInt.fromString('0')
+    rewardEntity = new Reward(id)
+    rewardEntity.amount = BigInt.fromString('0')
   }
 
   rewardEntity.owner = entity.owner
   rewardEntity.rewardAddress = eternalFarming.rewardToken
   rewardEntity.amount = rewardEntity.amount.plus(event.params.rewardAmount)
-  rewardEntity.save();  
+  rewardEntity.transaction = transaction.id
+  rewardEntity.timestamp = transaction.timestamp
+  rewardEntity.save()  
 
-
-  id =  eternalFarming.bonusRewardToken.toHexString() + entity.owner.toHexString()
+  id = eternalFarming.bonusRewardToken.toHexString() + entity.owner.toHexString()
   rewardEntity = Reward.load(id)
 
   if (rewardEntity == null){
@@ -197,8 +222,8 @@ export function handleCollect( event: RewardsCollected): void{
   rewardEntity.owner = entity.owner
   rewardEntity.rewardAddress = eternalFarming.bonusRewardToken
   rewardEntity.amount = rewardEntity.amount.plus(event.params.bonusRewardAmount)
-  rewardEntity.save();
-}
-}
+  rewardEntity.transaction = transaction.id
+  rewardEntity.timestamp = transaction.timestamp
+  rewardEntity.save()
 } 
 
